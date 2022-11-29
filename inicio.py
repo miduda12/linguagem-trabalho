d46f16d0cceb4b2c44f824ff551d66cef00f1340
miduda12 <108310019+miduda12@users.noreply.github.com>
2022-11-28 22:53:41 -0400

    Add files via upload

---
+++ inicio.py
@@ -0,0 +1,146 @@
+A = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7'] 
+B = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7'] 
+C = ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7'] 
+
+print('A  B  C') 
+
+
+for i in range(7): 
+    print(A[i] + ' ' + B[i] + ' ' + C[i]) 
+print('A  B  C')
+
+
+nun1 = input(' Digite a coluna escolhida: ') 
+
+
+print('A  B  C')
+
+if nun1 == 'A':     
+    LI = B + A + C  
+                    
+if nun1 == 'B':
+    LI = A + B + C
+
+if nun1 == 'C':
+    LI = A + C + B
+
+A = [] 
+B = [] 
+C = [] 
+
+print('A  B  C')
+
+
+for i in range(1):
+    A.append(LI[0])
+    B.append(LI[1])
+    C.append(LI[2])
+    A.append(LI[3])
+    B.append(LI[4])
+    C.append(LI[5])
+    A.append(LI[6])
+    B.append(LI[7])
+    C.append(LI[8])
+    A.append(LI[9])
+    B.append(LI[10])
+    C.append(LI[11])
+    A.append(LI[12])
+    B.append(LI[13])
+    C.append(LI[14])
+    A.append(LI[15])
+    B.append(LI[16])
+    C.append(LI[17])
+    A.append(LI[18])
+    B.append(LI[19])
+    C.append(LI[20])
+
+
+for i in range(7):
+    print(A[i] + ' ' + B[i] + ' ' + C[i])
+print('A  B  C')
+
+nun1 = input('Digite a coluna escolhida: ')  
+
+if nun1 == 'A':
+    LI = B + A + C
+
+if nun1 == 'B':
+    LI = A + B + C
+
+if nun1 == 'C':
+    LI = A + C + B
+
+A = []
+B = []
+C = []
+
+print('A  B  C')
+
+
+for i in range(1):
+    A.append(LI[0])
+    B.append(LI[1])
+    C.append(LI[2])
+    A.append(LI[3])
+    B.append(LI[4])
+    C.append(LI[5])
+    A.append(LI[6])
+    B.append(LI[7])
+    C.append(LI[8])
+    A.append(LI[9])
+    B.append(LI[10])
+    C.append(LI[11])
+    A.append(LI[12])
+    B.append(LI[13])
+    C.append(LI[14])
+    A.append(LI[15])
+    B.append(LI[16])
+    C.append(LI[17])
+    A.append(LI[18])
+    B.append(LI[19])
+    C.append(LI[20])
+
+for i in range(7):
+    print(A[i] + ' ' + B[i] + ' ' + C[i])
+print('A  B  C')
+
+nun1 = input('Digite a coluna escolhida: ') 
+
+if nun1 == 'A':
+    LI = B + A + C
+
+if nun1 == 'B':
+    LI = A + B + C
+
+if nun1 == 'C':
+    LI = A + C + B
+
+A = []
+B = []
+C = []
+
+for i in range(1):
+    A.append(LI[0])
+    B.append(LI[1])
+    C.append(LI[2])
+    A.append(LI[3])
+    B.append(LI[4])
+    C.append(LI[5])
+    A.append(LI[6])
+    B.append(LI[7])
+    C.append(LI[8])
+    A.append(LI[9])
+    B.append(LI[10])
+    C.append(LI[11])
+    A.append(LI[12])
+    B.append(LI[13])
+    C.append(LI[14])
+    A.append(LI[15])
+    B.append(LI[16])
+    C.append(LI[17])
+    A.append(LI[18])
+    B.append(LI[19])
+    C.append(LI[20])
+
+
+print('Sua escolha foi o termo: ',LI[10])
